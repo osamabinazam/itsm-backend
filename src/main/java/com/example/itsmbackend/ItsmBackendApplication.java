dbb6043d0dcf0434863cba1521cf20fae6b7df49
Osama Bin Azam <osamabinazam@gmail.com>
2024-09-09 00:46:06 +0500

    first commit

---
+++ src/main/java/com/example/itsmbackend/ItsmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.itsmbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItsmBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItsmBackendApplication.class, args);
+    }
+
+}
