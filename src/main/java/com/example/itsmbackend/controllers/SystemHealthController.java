dc9b5155f06cbe6c9252f27b1c5a059ae5f6c5a0
Osama Bin Azam <osamabinazam@gmail.com>
2024-09-09 12:59:15 +0500

    System health check

---
+++ src/main/java/com/example/itsmbackend/controllers/SystemHealthController.java
@@ -0,0 +1,17 @@
+package com.example.itsmbackend.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/health")
+public class SystemHealthController {
+
+
+    @GetMapping
+    String healthCheck(){
+        return "System is up and running.";
+    }
+}
