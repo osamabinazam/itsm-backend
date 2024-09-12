8398ae9c14efb17aa87cfaccda04b7d579e39f89
Osama Bin Azam <osamabinazam@gmail.com>
2024-09-13 01:24:48 +0500

    Switch to Postgres

--- src/main/java/com/example/itsmbackend/ItsmBackendApplication.java
+++ src/main/java/com/example/itsmbackend/ItsmBackendApplication.java
@@ -3,7 +3,7 @@ package com.example.itsmbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.itsmbackend")
 public class ItsmBackendApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/itsmbackend/controllers/SystemHealthController.java
+++ src/main/java/com/example/itsmbackend/controllers/SystemHealthController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/health")
+@RequestMapping("/public/api/health")
 public class SystemHealthController {
 
 
